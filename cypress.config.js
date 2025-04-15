0ad0c65046e5c9b04cdb6405c320c5a00babf19c
hannusia <ye.anna.yu@gmail.com>
2025-04-15 10:45:55 +0300

    update workflows

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
